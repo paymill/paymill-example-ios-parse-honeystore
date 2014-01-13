9d305e61100d1a2d6faf6e72c1de631c75038e7d
Vladimir Marinov <vladimir.marinov@qaiware.com>
2014-01-13 14:00:12 +0200

    add business layer

---
+++ BusinessLogic/Controller.m
@@ -0,0 +1,13 @@
+//
+//  Controller.m
+//  Honey on Sale
+//
+//  Created by Lubomir Velkov on 13.01.14.
+//  Copyright (c) 2014 г. Vladimir Marinov. All rights reserved.
+//
+
+#import "Controller.h"
+
+@implementation Controller
+
+@end
